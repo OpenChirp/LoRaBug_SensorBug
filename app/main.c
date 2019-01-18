61051c19a56ddcb4ed4aed79bac5d07f2e8822a6
Craig Hesling <craig@hesling.com>
2019-01-18 17:10:26 -0500

    Bumped firmware version to 2.12

--- app/main.c
+++ app/main.c
@@ -57,9 +57,9 @@ static Char mainTaskStack[TASKSTACKSIZE];
 static Event_Struct runtimeEventsStruct;
 static Event_Handle runtimeEvents;
 
-// Software Version 2.11
+// Software Version 2.12
 const uint32_t software_ver_major = 2;
-const uint32_t software_ver_minor = 11;
+const uint32_t software_ver_minor = 12;
 
 /*------------------------------------------------------------------------*/
 /*                     Configuration                                      */
